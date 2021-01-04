b3c850cffd33cbac795a36e58c36ee962b5867f1
ElMoustaphaoui <46606940+El_Moustaphaoui@users.noreply.github.com>
2021-01-04 01:38:41 +0000

    Handling Server error

--- src/app/repositories/repositories-list/repositories-list.component.ts
+++ src/app/repositories/repositories-list/repositories-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 import {RepoRestService } from '../../services/repo-repo-rest.service'
 import { Repository} from '../models/repository.model';
 
@@ -12,6 +12,7 @@ export class RepositoriesListComponent implements OnInit {
 
   repositories: Repository[] | null;
   isLoading: boolean;
+  erreurLoading: boolean;
   number_of_pages:number;
   reposinit: Subscription;
   reposPage: Subscription;
@@ -30,6 +31,7 @@ export class RepositoriesListComponent implements OnInit {
   };
   constructor(private repoRestService: RepoRestService) {
 
+       //The first pagination config
        this.config = {
           itemsPerPage: 10,
           currentPage: 1, 
@@ -45,8 +47,10 @@ export class RepositoriesListComponent implements OnInit {
   }
 
   fetchReposPage(page:number): void {
+
     //waiting the api response
     this.isLoading =true;
+    this.erreurLoading=false;
     //calling the github api 
     this.reposPage=this.repoRestService.getreposPage(page).subscribe((data:any) =>{
           
@@ -55,19 +59,27 @@ export class RepositoriesListComponent implements OnInit {
           this.config.totalItems=data.total_count;
           // passing the repo data to UI
           this.repositories=data.items;
-          //now once the we get data from api we made loading down
+          //now once the we get data from api we make loading down
           this.isLoading =false;         
     
     },
     err =>{
+          //We need to show erreur in case serveur down or no internet connection
+          this.erreurLoading=true;
           console.log(" erreur while get repos data of next page : " + err);
+          //If we don"t get data from server we stop loading : we show erreur !
+          this.isLoading =false; 
+          this.handleError('500 SERVER ERROR, CONTACT ADMINISTRATOR !')
+          
     })
 }
 
 
   fetchRepos(): void {
+
         //waiting the api response
         this.isLoading =true;
+        this.erreurLoading=false;
         //calling the github api 
         this.reposinit=this.repoRestService.getreposInit().subscribe((data:any) =>{
     
@@ -75,14 +87,22 @@ export class RepositoriesListComponent implements OnInit {
               this.config.totalItems=data.total_count;
               // passing the repo data to UI
               this.repositories=data.items;
-              //now once the we get data from api we made loading down
+              //now once the we get data from api we make loading down
               this.isLoading =false;         
         
         },
         err =>{
               console.log(" erreur while get repos data : " + err);
+              //If we don"t get data from server we stop loading : we show erreur !
+              this.isLoading =false; 
+              this.handleError('500 SERVER ERROR, CONTACT ADMINISTRATOR !')
         })
   }
+  //Take care of some erreur 
+  handleError (err) {
+    alert(err);
+  }
+  
 
   ngOnDestroy() {
     //Good habit are always good 
