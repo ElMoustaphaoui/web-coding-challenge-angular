dacfd9d7fab4e68db1e000de6a70f9cee0904434
El Moustaphaoui Oussama <oussama.elmoustaphaoui@nucomat.be>
2021-09-05 11:34:45 +0100

    get last 30 days repos

--- src/app/services/repo-repo-rest.service.ts
+++ src/app/services/repo-repo-rest.service.ts
@@ -10,17 +10,22 @@ import { Repository } from '../repositories/models/repository.model';
 export class RepoRestService {
   //To get the most starred Github repos created in the last 30 days (relative to 2017-11-22)
   url = environment.githubRestUrl;
+  lastMonth = new Date(new Date().setMonth(new Date().getMonth() - 1)).toISOString().split('T')[0];
 
   constructor(private http: HttpClient) { }
 
   // i had limited the row to 10 per page
   //get the date from github rest api for the first time
   getreposInit() {
-    return this.http.get<Repository[]>(`${this.url}` + `&pag=` + `${1}`);
+
+
+    return this.http.get<Repository[]>(`${this.url}` + `?q=created:>` + `${this.lastMonth}` + `&sort=stars&order=desc&per_page=10` + `&page=` + `${1}`);
+
+
   }
   //get date for specifique page
   getreposPage(page: number) {
-    return this.http.get<Repository[]>(`${this.url}` + `&pag=` + `${page}`);
+    return this.http.get<Repository[]>(`${this.url}` + `?q=created:>` + `${this.lastMonth}` + `&sort=stars&order=desc&per_page=10` + `&page=` + `${page}`);
   }
 
 }
